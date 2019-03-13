254bb05d78bc0b0e5d02f67e564a53b2de43aa87
ZmicerOk <vasikou73@gmail.com>
2019-03-13 22:40:41 +0300

    feat: the task is done

--- src/index.js
+++ src/index.js
@@ -1,32 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    
-// makeExchange = function (currency){
-//     // Your code goes here!
-//     // Return an object containing the minimum number of coins needed to make change
 
-    // const H = {"H":50}, Q = {"Q":25}, D = {"D":10}, N = {"N":5}, P = {"P":1};
-    //let amount = {H, Q, D, N, P};
-    let H=0, Q=0, D=0, N=0, P=0;
-    let amount = {};
-    if (currency <= 0) return {};
-    if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
-    if ((currency/50)>1) {
-        let H=parseInt(currency/50);
-        if ((currency%50)==0){
-        
-           // currency=currency-H*50;
-           // if ((currency/25)>1) {
-            //   let Q=parseInt(currency/25);
-            }
-        }
-        
-     
-     return {"H":H, "Q":Q, "D":D, "N":N, "P":P};
-//     if (currency/amount[H]>=0) return amount[H] = parseInt(currency/amount[H]);
-//     console.log (amount[H]);
-    // if (currency%50>=0) return {"H":parseInt(currency/50)};
-    // if (currency%25>=0) return {"Q":parseInt(currency/25)};
+    var H=parseInt(currency/50);
+    var Q=0, D=0, N=0, P=0;
+    var amount = {};
+        if (currency <= 0) return {};
+        if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+          currency=currency - H*50;
+          Q=parseInt(currency/25);
+          currency=currency - Q*25;
+          D=parseInt(currency/10);
+          currency=currency - D*10;
+          N=parseInt(currency/5);
+          currency=currency - N*5;
+          P=parseInt(currency);
     
-    //console.log (amount);
+        if (H>0) amount["H"]=H;
+        if (Q>0) amount["Q"]=Q;
+        if (D>0) amount["D"]=D;
+        if (N>0) amount["N"]=N;
+        if (P>0) amount["P"]=P;
+        return amount;
 }
